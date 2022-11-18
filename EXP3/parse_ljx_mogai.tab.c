167ffbe5ddd357ce8af58fd048d265c792bc7708
Lacrimosa-minor <576533584@qq.com>
2022-11-18 22:04:16 +0800

    debug

--- EXP3/parse_ljx_mogai.tab.c
+++ EXP3/parse_ljx_mogai.tab.c
@@ -2500,7 +2500,7 @@ yyreturn:
 int main(int argc, char** argv)
 {
 	// if(argc <= 1) return 1;
-	FILE* f = fopen("test7.cmm", "r");
+	FILE* f = fopen("test5.cmm", "r");
 	// if(!f){
 		// perror(argv[1]);
 		// return 1;
