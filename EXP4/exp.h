abf9576dfc38b827445ddde2e8c8c2eaa08a903b
SpaceBall0929 <liujunxin0929@gmail.com>
2022-12-03 17:35:13 +0800

    modified:   EXP4/exp.h 	modified:   EXP4/exp.c

--- EXP4/exp.h
+++ EXP4/exp.h
@@ -30,12 +30,20 @@ operand* Exp_s(treeNode *exp);
         operation* binary(treeNode *t);
         //处理关系运算，返回一个参数表（因为布尔运算的符号不算符号，而是直接当操作数来用了）
         operand_list bool(treeNode *t, int opnum);
+        //处理and or not这三种情况
+        operation* and_or_not(treeNode*t, int type);
 
     //3-处理MINUS Exp，NOT Exp,lp exp rp直接在exp里处理
     operand* o_exp(treeNode *tn1, treeNode *tn2, treeNode *tn3);
         //处理单元运算，输入树的节点，输出生成的operation指针
         operation* unary(treeNode *t);
-
+    //4-处理函数 无参&有参
+    operand* fun_no_args(treeNode *tn1, treeNode *tn2, treeNode *tn3);
+    operand* fun_with_args(treeNode *tn1, treeNode *tn2, treeNode *tn3, treeNode *tn4);
+    //5-处理结构体
+    operand* exp_st(treeNode *tn1, treeNode *tn2, treeNode *tn3);
+    //6-处理数组
+    operand* exp_ar(treeNode *tn1, treeNode *tn2, treeNode *tn3, treeNode *tn4);
 
 //返回是不是这个type
 int check_type(treeNode* t, int i);
