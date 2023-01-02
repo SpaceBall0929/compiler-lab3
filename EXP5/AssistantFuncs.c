c4017c3befd65e2c3b3d36a79beaf95ec588a519
Lacrimosa-minor <576533584@qq.com>
2023-01-02 10:28:10 +0800

    build the final part exp5

---
+++ EXP5/AssistantFuncs.c
@@ -0,0 +1,29 @@
+# include <math.h>
+
+
+int IsPrime(int num)
+{
+	if (num == 1)
+		return 0;
+	if (num == 2)
+		return 1;
+	for (int i = 2; i <= sqrt(num); i++)
+	{
+		if (num % i == 0)
+			return 0;
+	}
+	return 1;
+}
+
+int GetClosestPrime(int num)
+{
+	if (num == 1)
+		return 1;
+	int p;
+	for (int i = 2;i <= num;i++)
+	{
+		if (IsPrime(i))
+			p = i;
+	}
+	return p;
+}
